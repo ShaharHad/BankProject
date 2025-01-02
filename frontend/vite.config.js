147da09e0eb81e704180b1431c02f7b3dfddf6ff
shahar.hadad <shaharhadad123@gmail.com>
2025-01-02 16:05:18 +0200

    update vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://shaharhad.github.io/BankProject/",
+  base: "https://shaharhad.github.io/BankProject/frontend",
 })
