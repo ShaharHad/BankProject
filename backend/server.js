02de1ba1758935e40db0cd8ea2cd881e2c4ab569
shahar.hadad <shaharhadad123@gmail.com>
2025-01-05 20:31:39 +0200

    update port

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const dbConnection = require('./db_connection/db_connection');
 
 dbConnection.on('connected', () => {
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 10000;
     app.listen(port, () => {
         console.log(`Server is running on http://localhost:${port}`);
     });
